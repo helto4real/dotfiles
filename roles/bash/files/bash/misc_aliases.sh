0907af4ca7e61012b9984d510f36b7ff17820975
Tomas Hellström <tomas.hellstrom@yahoo.se>
2024-02-16 22:31:10 +0100

    correct-explorer-ling

--- roles/bash/files/bash/misc_aliases.sh
+++ roles/bash/files/bash/misc_aliases.sh
@@ -3,5 +3,5 @@
 alias bt='bpytop'
 alias update='sudo nala update && sudo nala upgrade -y && sudo nala autoremove -y'
 alias comment-header='toilet -f pagga -S'
-alias i='explorer.exe'
+alias i='/mnt/c/Windows/explorer.exe'
 alias ncdu='ncdu --color dark -e -q --exclude-caches --exclude-kernfs -L'
