5027f6a6251cbd1ce2ea0d15af061ba314d84096
Tomas Hellström <tomas.hellstrom@yahoo.se>
2024-02-17 10:13:36 +0100

    outdated-dotnet-tool

--- roles/bash/files/bash/paths_vars.sh
+++ roles/bash/files/bash/paths_vars.sh
@@ -6,5 +6,7 @@ addToPath /usr/local/go/bin
 addToPath $HOME/.dotfiles/bin
 #addToPath $HOME/.cargo/bin
 addToPathFront $HOME/.local/bin
+# Add paths to dotnet tools
+addToPath $HOME/.dotnet/tools
 # ccache
 #addToPathFront /usr/lib/ccache
