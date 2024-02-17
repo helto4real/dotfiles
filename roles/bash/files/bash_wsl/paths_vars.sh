2e0d01bf6fc069d4b0c6f9a81095e63c80b423f3
Tomas Hellström <tomas.hellstrom@yahoo.se>
2024-02-17 08:34:31 +0100

    paths-for-wsl

---
+++ roles/bash/files/bash_wsl/paths_vars.sh
@@ -0,0 +1,10 @@
+
+#!/usr/bin/env bash
+# Make windows specific paths to work for 
+# - code
+# - explorer.exe
+addToPath '/mnt/c/Users/tomas/AppData/Local/Programs/Microsoft VS Code/bin/'
+
+addToPath '/mnt/c/Windows/System32/WindowsPowerShell/v1.0/'
+
+addToPath '/mnt/c/Windows/'
