324e0be60661b1a61db9693a0e1511c664613243
Tomas Hellström <tomas.hellstrom@yahoo.se>
2024-02-17 16:59:26 +0100

    lazygit-alias

--- roles/bash/files/bash/misc_aliases.sh
+++ roles/bash/files/bash/misc_aliases.sh
@@ -5,3 +5,4 @@ alias update='sudo nala update && sudo nala upgrade -y && sudo nala autoremove -
 alias comment-header='toilet -f pagga -S'
 alias i='/mnt/c/Windows/explorer.exe'
 alias ncdu='ncdu --color dark -e -q --exclude-caches --exclude-kernfs -L'
+alias lzg='lazygit'
