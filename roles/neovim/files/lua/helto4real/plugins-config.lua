c6d160dc469d9e77f7735c2170cbb8a57e7e4ad6
Tomas Hellström <tomas.hellstrom@yahoo.se>
2024-02-24 00:39:49 +0100

    add folke-noice plugin for neovim

--- roles/neovim/files/lua/helto4real/plugins-config.lua
+++ roles/neovim/files/lua/helto4real/plugins-config.lua
@@ -218,6 +218,21 @@ require('lazy').setup(
             "lewis6991/gitsigns.nvim"
         },
         { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
+        {
+          "folke/noice.nvim",
+          event = "VeryLazy",
+          opts = {
+            -- add any options here
+          },
+          dependencies = {
+            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+            "MunifTanjim/nui.nvim",
+            -- OPTIONAL:
+            --   `nvim-notify` is only needed, if you want to use the notification view.
+            --   If not available, we use `mini` as the fallback
+            "rcarriga/nvim-notify",
+          }
+        },
         require 'helto4real.plugins.debug',
 
         -- Detect tabstop and shiftwidth automatically
