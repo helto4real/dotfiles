4ff616ae12329d9272ca5a3aa9ef0427f4063945
Tomas Hellström <tomas.hellstrom@yahoo.se>
2024-02-26 12:53:44 +0100

    Remove some conflicting keybindings

--- roles/neovim/files/lua/helto4real/plugins/dev/lsp.lua
+++ roles/neovim/files/lua/helto4real/plugins/dev/lsp.lua
@@ -40,20 +40,22 @@ return {
             nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
             nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
             nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+            nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
             nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
             nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-            nmap('<leader>wws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+            nmap('<leader>gs', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
             nmap('<leader>I', require('telescope.builtin').lsp_incoming_calls, '[I]ncoming calls')
             -- See `:help K` for why this keymap
             nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-            nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+            nmap('<leader>k', vim.lsp.buf.signature_help, 'Signature Documentation')
             -- Lesser used LSP functionality
-            nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-            nmap('<leader>wwa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-            nmap('<leader>wwr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-            nmap('<leader>wwl', function()
-                print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-            end, '[W]orkspace [L]ist Folders')
+
+            -- Workspace related LSP functionality, not used often
+            -- nmap('<leader>wwa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+            -- nmap('<leader>wwr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+            -- nmap('<leader>wwl', function()
+            --     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+            -- end, '[W]orkspace [L]ist Folders')
             -- Create a command `:Format` local to the LSP buffer
             vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
                 vim.lsp.buf.format()
