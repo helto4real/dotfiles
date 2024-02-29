19deedc15e86e8f20e9dc3586df635c34bd566e0
Tomas Hellström <tomas.hellstrom@yahoo.se>
2024-02-29 21:33:55 +0100

    Add multi-cursor support in NeoVim

--- roles/neovim/files/lua/helto4real/plugins/dev/dotnet-tools.lua
+++ roles/neovim/files/lua/helto4real/plugins/dev/dotnet-tools.lua
@@ -3,5 +3,31 @@ return {
     dependencieset = {
         "plenary.nvim",
     },
-    opts = {}
+    opts = {},
+    keys = {
+        {
+            "<leader>ct",
+            mode = { "n", "o", "x" },
+            ":DotNetToolsTest<CR>",
+            desc = "Dotnet test"
+        },
+        {
+            "<leader>co",
+            mode = { "n", "o", "x" },
+            ":DotNetToolsOutdated<CR>",
+            desc = "Dotnet outdated"
+        },
+        {
+            "<leader>cu",
+            mode = { "n", "o", "x" },
+            ":DotNetToolsOutdatedUpgrade<CR>",
+            desc = "Dotnet outdated upgrade"
+        },
+        {
+            "<leader>cb",
+            mode = { "n", "o", "x" },
+            ":DotNetToolsBuild<CR>",
+            desc = "Dotnet build"
+        },
+    },
 }
