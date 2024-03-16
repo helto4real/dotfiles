008e48e4262f96dc28c5cd496c18d72c9077a63f
Tomas Hellström <tomas.hellstrom@yahoo.se>
2024-03-16 09:27:41 +0100

    added floaterm and removed lazygit

---
+++ roles/neovim/files/lua/helto4real/plugins/floaterm.lua
@@ -0,0 +1,11 @@
+return {
+    'voldikss/vim-floaterm',
+        keys = {
+        {
+            "<leader>gg",
+            ":FloatTermNew lazygit<CR>",
+            desc = "Lazygit"
+        },
+    },
+
+}
