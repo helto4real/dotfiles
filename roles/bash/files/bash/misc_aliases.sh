b2b82bccccfefd71e3c89e072a678652670096a9
Tomas Hellström <tomas.hellstrom@yahoo.se>
2024-04-21 08:50:18 +0200

    Add eza default ls

--- roles/bash/files/bash/misc_aliases.sh
+++ roles/bash/files/bash/misc_aliases.sh
@@ -7,3 +7,6 @@ alias i='/mnt/c/Windows/explorer.exe'
 alias ncdu='ncdu --color dark -e -q --exclude-caches --exclude-kernfs -L'
 alias tm='tmux'
 alias lzg='lazygit'
+
+# ---- Eza (better ls) -----
+alias ls="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
