17d50b33ea432f3d03f234989af03452f0052b28
Tomas Hellström <tomas.hellstrom@yahoo.se>
2024-07-08 22:17:56 +0200

    forgot cr

--- roles/neovim/files/lua/helto4real/core/keymaps.lua
+++ roles/neovim/files/lua/helto4real/core/keymaps.lua
@@ -72,7 +72,7 @@ map("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 
 map("n", "<leader>le", "<cmd>setlocal spell spelllang=en_us<CR>", "Enable spell checking en_us")
 map("n", "<leader>ls", "<cmd>setlocal spell spelllang=sv_se<CR>", "Enable spell checking sv_se")
-map("n", "<leader>ld", "<cmd>setlocal nospell", "Disable spell checking")
+map("n", "<leader>ld", "<cmd>setlocal nospell<CR>", "Disable spell checking")
 
 -- smart replacing the current word
 map("n", "<leader>vpp", "<cmd>e");
