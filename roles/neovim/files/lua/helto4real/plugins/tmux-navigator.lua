872a0c0f039048f0099f8f2819c920b8a961019f
Tomas Hellström <tomas.hellstrom@yahoo.se>
2024-09-29 20:22:51 +0200

    tmux-navigator: fix keybindings for tmux 3.2

--- roles/neovim/files/lua/helto4real/plugins/tmux-navigator.lua
+++ roles/neovim/files/lua/helto4real/plugins/tmux-navigator.lua
@@ -1,4 +1,26 @@
 return {
     'christoomey/vim-tmux-navigator',
+     keys = {
+          {
+            "<c-h>",
+            "<cmd><C-U>TmuxNavigateLeft<cr>",
+            desc="Tmux Navigate Left"
+          },
+          {
+            "<c-j>",
+            "<cmd><C-U>TmuxNavigateDown<cr>",
+            desc="Tmux Navigate Down"
+          },
+          {
+            "<c-k>",
+            "<cmd><C-U>TmuxNavigateUp<cr>",
+            desc="Tmux Navigate Up"
+          },
+          {
+            "<c-l>",
+            "<cmd><C-U>TmuxNavigateRight<cr>",
+            desc="Tmux Navigate Right"
+          },
+        },
     event = "VeryLazy",
 }
