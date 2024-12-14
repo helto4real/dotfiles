e8c1552347b68cd34619735fdaa6bc057e7e719f
Tomas Hellström <tomas.hellstrom@yahoo.se>
2024-12-14 18:55:55 +0100

    Just use telescope defaults

--- roles/neovim/files/lua/helto4real/plugins/telescope.lua
+++ roles/neovim/files/lua/helto4real/plugins/telescope.lua
@@ -25,17 +25,17 @@ return {
                     require('telescope.themes').get_dropdown(),
                 },
             },
-            defaults = {
-                mappings = {
-                    i = {
-                        ['<C-u>'] = false,
-                        ['<C-d>'] = false,
-                        ['<C-j>'] = actions.move_selection_next,
-                        ['<C-k>'] = actions.move_selection_previous,
-                        ['<C-q>'] = actions.smart_send_to_qflist,
-                    },
-                },
-            },
+            -- defaults = {
+            --     mappings = {
+            --         i = {
+            --             ['<C-u>'] = false,
+            --             ['<C-d>'] = false,
+            --             ['<C-j>'] = actions.move_selection_next,
+            --             ['<C-k>'] = actions.move_selection_previous,
+            --             ['<C-q>'] = actions.smart_send_to_qflist,
+            --         },
+            --     },
+            -- },
             find_files = {
 
                 hidden = true
