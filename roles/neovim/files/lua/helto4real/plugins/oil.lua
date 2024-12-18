1d081c4b58c19a9d8b3922ee4c3476ee1f415bd2
Tomas Hellström <tomas.hellstrom@yahoo.se>
2024-12-18 17:07:31 +0100

    Add Oil plugin

---
+++ roles/neovim/files/lua/helto4real/plugins/oil.lua
@@ -0,0 +1,21 @@
+return
+{
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+    config = function()
+        -- set keymaps
+        local keymap = vim.keymap -- for conciseness
+
+        keymap.set(
+            "n",
+            "-",
+            "<cmd>Oil<cr>",
+            { desc = "Open Oil file manager" }
+        )
+    end,
+}
